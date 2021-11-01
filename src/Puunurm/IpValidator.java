3be786a0b5c1c7ae764a56b68285104661afc522
opilane <opilane@HP-CZC6297B83.tthk.ee>
2021-11-01 09:28:50 +0200

    Req1 - assert empty string - fail

---
+++ src/Puunurm/IpValidator.java
@@ -0,0 +1,10 @@
+package Puunurm;
+
+import java.util.Arrays;
+
+public class IpValidator {
+	
+	public Boolean ValidateIpv4Address(String string) {
+		return true;
+	}
+}
---
+++ src/Puunurm/ipValidatorTests.java
@@ -0,0 +1,28 @@
+package Puunurm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ipValidatorTests {
+
+	private IpValidator validator;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		validator = new IpValidator();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void shouldReturn_False_GivenEmptyStringParameter() {
+		assertFalse(validator.ValidateIpv4Address(""));
+	}
+
+
+}
